2c163fe503ca85f59f9b53fc80bde104c152510f
Simran Makhijani <simranmakhijani55@gmail.com>
2022-10-29 00:17:50 +0530

    added 67. Add Binary.cpp

---
+++ 67. Add Binary.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string sum = "";
+        
+        int carry = 0, i = a.size() - 1, j = b.size() - 1;
+        while(i >= 0 || j >= 0 || carry != 0)
+        {
+            carry += i >= 0 ? a[i] - '0' : 0;
+            carry += j >= 0 ? b[j] - '0' : 0;
+            sum += carry % 2 + '0';
+            carry /= 2;
+            i--,j--;
+        }
+        reverse(sum.begin(), sum.end());
+        
+        return sum;
+    }
+};
